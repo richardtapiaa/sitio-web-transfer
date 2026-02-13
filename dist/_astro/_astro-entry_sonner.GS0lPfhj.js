6c52c3c3182081a39aeddd1523fdcb03ef423535
stafno tapia <psrichard30@gmail.com>
2026-02-12 20:00:57 -0600

    fix seo

--- dist/_astro/_astro-entry_sonner.GS0lPfhj.js
+++
@@ -1,1 +0,0 @@
-import{T as p}from"./_astro-entry_sonner.BALpnfNA.js";import"./index.BIjdzmyy.js";import"./index.tYu8WCzq.js";export{p as Toaster};
---
+++ dist/_astro/_astro-entry_sonner.n8X1ZPH8.js
@@ -0,0 +1,1 @@
+import{T as p}from"./_astro-entry_sonner.CKQCVWwI.js";import"./index.Chpzn5vo.js";import"./index.BHKNQgFg.js";export{p as Toaster};
